25be6d95286484af2e902177c0b76522f07805bd
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-01-29 19:45:42 +0500

    Just added the animated letters functionality

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,18 @@
 import './index.scss';
 import { Link } from 'react-router-dom'
+
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 const Home = ()=>{
+    const [letterClass,setLetterClass] = useState('text-animate')
+    const nameArray = ['R','a','f','a','y',',']
+    const workArray = ['W','e','b',' ','E','n','g','i','n','e','e','r']
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-            <h1>Hello, <br/> 
-            I'm Rafay,
+            <h1>Hello, <br/>I'm  <AnimatedLetters strArray={nameArray} letterClass={letterClass} index={15}/>
             <br/>
-            Web Engineer.
+            <AnimatedLetters strArray={workArray} letterClass={letterClass} index={22} />
             </h1>
             <h2>MERN Stack Web Engineer / Javascript / Freelancer</h2>
             <Link to="/contact" className='flat-button'>CONTACT ME</Link>
