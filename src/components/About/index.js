69e4c047e3cee500d5868e1f967ddbb2649069ef
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-02-01 01:08:15 +0500

    About page completion Text+Animation plus beautiful cube of skills

---
+++ src/components/About/index.js
@@ -0,0 +1,64 @@
+import { useState,useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faNodeJs, faReact } from '@fortawesome/free-brands-svg-icons'
+
+const About = () => {
+    const aboutMeArray = ['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']
+    const [letterClass, setLetterClass] = useState('text-animate');
+    useEffect(() => {
+ 
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+
+        return () => clearTimeout(timeoutId);
+    }, []);
+   
+    return (
+        <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters strArray={aboutMeArray} letterClass={letterClass} index={50} />
+                </h1>
+                <p>
+                    Hello, I'm Rafay, a software engineering student in 3rd year.
+                    I have proven academic excellence in programming fundamentals, OOP,Database Systems and Requirement Engineering.
+                </p>
+                <p> I specialize in creating efficient and user-friendly websites using the MERN stack (ReactJS, NextJS, NodeJS, ExpressJS, and MongoDB).
+                    Alongside my technical skills, I have a strong understanding of the software engineering life cycle, ensuring that development is not only efficient but also maintains high quality.</p>
+                <p>
+                    If I were to encapsulate myself in a single sentence, I would describe me as a problem solver, inherently curious, and a firm believer in the efficacy of smart work
+                </p>
+            </div>
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                   <div className='face1'>
+                   <FontAwesomeIcon icon={faReact} color='#DD0031' />
+                   </div>
+                   <div className='face2'>
+                   <FontAwesomeIcon icon={faNodeJs} color='#F06529' />
+                   </div>
+                   <div className='face3'>
+                   <FontAwesomeIcon icon={faHtml5} color='#28A4D9' />
+                   </div>
+                   <div className='face4'>
+                   <FontAwesomeIcon icon={faJsSquare} color='#5ED4F4' />
+                   </div>
+                   <div className='face5'>
+                   <FontAwesomeIcon icon={faCss3} color='#EFD81D' />
+                   </div>
+                   <div className='face6'>
+                   <FontAwesomeIcon icon={faGitAlt} color='#EC4E28' />
+                   </div>
+                </div>
+            </div>
+        </div>
+        </>
+        
+    )
+}
+
+export default About 
