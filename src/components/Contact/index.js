19842346548b0cf11e709d25f25d02ca0e3c32e7
Syed Muhammad Rafay Hassni <100311424+rafaysyed1@users.noreply.github.com>
2024-02-06 19:45:38 +0500

    Added email js email funcationality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,10 +1,14 @@
 import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 const Contact = () => {
     const contactMeArray = ['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']
-    const [letterClass, setLetterClass] = useState('text-animate')
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const token = process.env.REACT_APP_EMAIL_JS_TOKEN;
+    const refForm = useRef(null); 
+
     useEffect(() => {
         const timeoutId = setTimeout(() => {
             setLetterClass('text-animate-hover');
@@ -12,9 +16,36 @@ const Contact = () => {
 
         return () => clearTimeout(timeoutId);
     }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log('Sending email...');
+        try {
+            emailjs
+            .sendForm(
+                'gmail',
+                'template_6r9smuf',
+                refForm.current,
+                'service_lcjpq0x'
+            )
+            .then(() => {
+                console.log('Email sent successfully');
+                alert("Email sent successfully");
+                window.location.reload(false);
+            })
+            .catch((error) => {
+                console.log('Error occurred while sending email:', error.message);
+                alert("Error occurred while sending email");
+                console.error('Error:', error);
+            });
+        } catch (error) {
+            console.log('Error occurred while sending email:', error.message);
+            alert("Error occurred while sending email");
+        }
+    };
     return (
         <>
-            <div className='container contact-page'>
+            <div className='container contact-page' style={{zIndex: 1}}> 
                 <div className='text-zone'>
                     <h1>
                         <AnimatedLetters strArray={contactMeArray} letterClass={letterClass} index={15} />
@@ -25,7 +56,7 @@ const Contact = () => {
                         questions, don't hesitate to contact me using below form either.
                     </p>
                     <div className="contact-form">
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className="half">
                                     <input placeholder="Name" type="text" name="name" required />
@@ -59,6 +90,13 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className='info-map'>
+                Rafay,
+                <br/>
+                Pakistan,
+                <br/>
+                
                 </div>
             </div>
             <Loader type="pacman" className="loader-active" />
